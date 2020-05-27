2382a54e5c7494675e855f23ae23ff9c59bc5072
HenSetiawan <56755687+HenSetiawan@users.noreply.github.com>
2020-05-27 19:46:10 +0700

    Add files via upload

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,1 +1,4 @@
-
+<div class="container">
+<h1 class="mt-4">About Me</h1>
+<p>Hallo Nama saya <?= $data['nama'] ?> saya Seorang <?= $data['pekerjaan'] ?></p>
+</div>
---
+++ app/views/about/pages.php
@@ -0,0 +1,1 @@
+<h1>Ini adalah Pages</h1>
