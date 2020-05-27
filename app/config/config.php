43c4618a70161746a7e97bf311ed9b76f606dff2
HenSetiawan <56755687+HenSetiawan@users.noreply.github.com>
2020-05-27 19:38:07 +0700

    Add files via upload

--- app/config/config.php
+++ app/config/config.php
@@ -1,1 +1,8 @@
+<?php 
+    const BASEURL = 'http://localhost/mvc/public';
 
+    // DB
+    const DB_HOST ='localhost';
+    const DB_USER ='hendy';
+    const DB_PASS ='lavender';
+    const DB_NAME ='phpmcv';
